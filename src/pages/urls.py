5471c0be28bd8eec101b8bb09253ff31c6f4b85a
PenguinPontes <franciscoppontes369@gmail.com>
2020-06-28 18:16:57 +0100

    Home page

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from .views import home_view, about_view, contact_view
+from .views import home_view, about_view, contact_view, projects_view
 
 app_name = "pages"
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', home_view),
     path('about/', about_view, name='about-view'),
     path('contact/', contact_view, name='contact-view'),
+    path('projects/', projects_view, name='projects-view'),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -16,6 +16,11 @@ def about_view(request, *args, **kwargs):
     return render(request, 'pages/about.html', {})
 
 
+def projects_view(request, *args, **kwargs):
+    """View for url about"""
+    return render(request, 'pages/projects.html', {})
+
+
 def contact_view(request, *args, **kwargs):
     """View for url contactus"""
     if request.method == "POST":
@@ -23,11 +28,12 @@ def contact_view(request, *args, **kwargs):
         message_email = request.POST['message_email']
         message_body = request.POST['message_body']
 
+        message_body = "Sender Email: " + message_email + "\nMensagem: " + message_body
         send_mail(
             message_name,
             message_body,
             message_email,
-            ['franciscoppontes369@gmail.com'])
+            ['franciscoppontes369@gmail.com', 'francisco.perestrelo.pontes@gmail.com'])
         return render(request, 'pages/contactus.html', {message_name: 'message_name', })
     else:
         return render(request, 'pages/contactus.html', {})
